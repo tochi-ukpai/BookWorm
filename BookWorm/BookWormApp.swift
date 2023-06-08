651f52e3fdc1ffe6b04e50edabe2c3985683d5df
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-06-08 10:33:49 +0100

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Theós on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
